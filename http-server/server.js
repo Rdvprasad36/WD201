b83497dd71a4282fce13a1e93a49d3a5188fdc7f
Rdvprasad36 <rdvprasad36@gmail.com>
2025-06-13 23:50:14 +0530

    Fix server.js to synchronously read files before starting server and serve /registration.html; ensure server listens on input port

--- http-server/server.js
+++ http-server/server.js
@@ -6,52 +6,29 @@ const minimist = require("minimist");
 const args = minimist(process.argv.slice(2));
 const port = args.port || 3000;
 
-let homeContent = "";
-let projectContent = "";
-let registrationContent = "";
-let stylesContent = "";
-let clientScriptContent = "";
-
-fs.readFile(path.join(__dirname, "home.html"), (err, data) => {
-  if (err) throw err;
-  homeContent = data;
-});
-
-fs.readFile(path.join(__dirname, "project.html"), (err, data) => {
-  if (err) throw err;
-  projectContent = data;
-});
-
-fs.readFile(path.join(__dirname, "registration.html"), (err, data) => {
-  if (err) throw err;
-  registrationContent = data;
-});
-
-fs.readFile(path.join(__dirname, "styles.css"), (err, data) => {
-  if (err) throw err;
-  stylesContent = data;
-});
-
-fs.readFile(path.join(__dirname, "index.js"), (err, data) => {
-  if (err) throw err;
-  clientScriptContent = data;
-});
+const homeContent = fs.readFileSync(path.join(__dirname, "home.html"));
+const projectContent = fs.readFileSync(path.join(__dirname, "project.html"));
+const registrationContent = fs.readFileSync(path.join(__dirname, "registration.html"));
+const stylesContent = fs.readFileSync(path.join(__dirname, "styles.css"));
+const clientScriptContent = fs.readFileSync(path.join(__dirname, "index.js"));
 
 const server = http.createServer((req, res) => {
   let url = req.url;
-  res.writeHead(200, { "Content-Type": "text/html" });
-
   switch (url) {
     case "/":
     case "/home":
     case "/home.html":
+      res.writeHead(200, { "Content-Type": "text/html" });
       res.end(homeContent);
       break;
     case "/project":
     case "/project.html":
+      res.writeHead(200, { "Content-Type": "text/html" });
       res.end(projectContent);
       break;
     case "/registration":
+    case "/registration.html":
+      res.writeHead(200, { "Content-Type": "text/html" });
       res.end(registrationContent);
       break;
     case "/styles.css":
