829aa197c9f7d8111cc5b27e9b1dca424c2844af
Rdvprasad36 <rdvprasad36@gmail.com>
2025-06-14 14:16:00 +0530

    Push all files with latest changes

---
+++ todo-cli/eslint.config.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: "latest",
+      },
+      globals: {
+        require: "readonly",
+        exports: "readonly",
+        module: "readonly",
+        jest: "readonly",
+      },
+    },
+    rules: {},
+  },
+];
