1e940f8096dd5b24188f8e35b2e37a4a463491ce
Rdvprasad36 <rdvprasad36@gmail.com>
2025-06-16 10:48:21 +0530

    Fix todo-app missing files with wd201-main template versions

---
+++ todo-app/index.js
@@ -0,0 +1,6 @@
+const app = require("./app"); 
+const listenport = process.env.PORT || 3000; 
+  
+app.listen(listenport, () => { 
+   console.log(`Started express server at port ${listenport}`); 
+ });
---
+++ todo-app/models/todo.js
@@ -0,0 +1,34 @@
+"use strict";
+const { Model } = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+
+  class Todo extends Model {
+        static associate() {
+     
+    }
+
+    static addTodo({ title, dueDate }) {
+      return this.create({ title: title, dueDate: dueDate, completed: false });
+    }
+
+    static getTodos() {
+      return this.findAll();
+    }
+    markAsCompleted() {
+      return this.update({ completed: true });
+    }
+  }
+  Todo.init(
+    {
+      title: DataTypes.STRING,
+      dueDate: DataTypes.DATEONLY,
+      completed: DataTypes.BOOLEAN,
+    },
+    {
+      sequelize,
+      modelName: "Todo",
+    },
+  );
+  return Todo;
+};
